4dfe519e36fb6956af13a14fca8dc9a5d19ca0a2
John Yuan <John.YCQ@outlook.com>
2019-05-03 23:07:47 +0800

    Using the keyword `function` to create functions

--- isFunction.js
+++ isFunction.js
@@ -6,8 +6,8 @@ var toString = Object.prototype.toString;
  * @param {any} it The variable to check
  * @returns {boolean} Returns `true` if the variable is a function, otherwise `false` is returned
  */
-var isFunction = function (it) {
+function isFunction(it) {
     return toString.call(it) === '[object Function]';
-};
+}
 
 module.exports = isFunction;
